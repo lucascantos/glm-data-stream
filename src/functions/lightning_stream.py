4fa32826792e9a5209a8c549aba6c89c945d75ab
Lucas Cantos <lucas.cantos@somarmeteorologia.com.br>
2021-04-21 17:57:01 -0300

    fix to payload

--- src/functions/lightning_stream.py
+++ src/functions/lightning_stream.py
@@ -22,8 +22,8 @@ class WeatherLightnings:
             if self.is_brazil(*coords):
                 payload.append(lightning_data)
                 if len(payload) >= 100:
-                    payload = []
                     self.sns.send(json.dumps({'lightnings': payload}), subject='lightnings')
+                    payload = []
         if len(payload)>0:
             self.sns.send(json.dumps({'lightnings': payload}), subject='lightnings')
         self.payload.extend(payload)
